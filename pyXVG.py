17b0896378f883dae8619746b1099f2d9a75109a
khabibki06 <48047638+khabibki06@users.noreply.github.com>
2019-08-07 11:36:09 +0700

    Update pyXVG.py

--- pyXVG.py
+++ pyXVG.py
@@ -10,12 +10,11 @@ except ImportError as e:
     sys.exit(1)
 
 def math_exp(text):
-    output = []
-    txt_split = text.split(" ")
-    for string in txt_split:
-        stringmath = "$" + string + "$"
-        output.append(stringmath)
-    return(" ".join(output))
+    if "\\xl\\f{}" in text:
+        text = text.replace("\\xl\\f{}", "$\\lambda$")
+    if "\\xD\\f{}" in text:
+        text = text.replace("\\xD\\f{}", "$\\Delta$")
+    return text
 
 def read_xvg(filename, save_metadata = True) :
     label = dict()
@@ -38,6 +37,9 @@ def read_xvg(filename, save_metadata = True) :
                 if "@ s" in line : 
                     y_legend = line.split("legend")[1].replace('\"','').strip()
                     legend.append(y_legend)
+                if "TYPE" in line :
+                    plotType = line.split(" ")[1].replace('\"','').strip()
+                    
             elif line.startswith("#"):
                 if save_metadata == True: 
                     meta.append(line.strip())
@@ -45,10 +47,12 @@ def read_xvg(filename, save_metadata = True) :
                 line = line.strip()
                 splitRowData =  list(filter(None, line.split(" ")))
                 data.append(splitRowData)
+    if plotType == "xydy" :
+        legend = legend + ["y", "dy"]
     dataset = pandas.DataFrame(data, columns=legend, dtype='float64')
-    return({'data' : dataset, 'label' : label, 'meta' : meta, 'legend' : legend})
+    return({'data' : dataset, 'label' : label, 'meta' : meta, 'legend' : legend, "plotType" : plotType})
 
-def plot_xvg(xvgdata, xlab = None, ylab = None, title = None, legend = None, average=False):
+def plot_xvg(xvgdata, xlab = None, ylab = None, title = None, legend = None, average=False, averageScale=20):
     if title == None :
         title = xvgdata['label']['title']
     if xlab == None :
@@ -57,16 +61,19 @@ def plot_xvg(xvgdata, xlab = None, ylab = None, title = None, legend = None, ave
         ylab = xvgdata['label']['ylab']
     if legend == None :
         legend = xvgdata['data'].iloc[:,1:].columns
-    #plt.plot(xvgdata['data'].iloc[:,0], xvgdata['data'].iloc[:,1:])
-    #plt.plot(xvgdata['data'].iloc[:,0], xvgdata['data'].iloc[:,1], label = xvgdata['legend'][1])
-    #plt.plot(xvgdata['data'].iloc[:,0], xvgdata['data'].iloc[:,1].rolling(window=20).mean(), label = "average" + xvgdata['legend'][1])
-    for ycol in xvgdata['legend']:
-        if not ycol == "x": 
-            plt.plot(xvgdata['data'].iloc[:,0], xvgdata['data'][ycol], label = ycol)
-            if average == True :
-                plt.plot(xvgdata['data'].iloc[:,0], xvgdata['data'][ycol].rolling(window=20).mean(), label = "average " + ycol)
+    if xvgdata['plotType'] == 'xy':
+        if average == True:
+            averageTimeStep = averageScale * (xvgdata['data'].iloc[1,0]- xvgdata['data'].iloc[0,0])
+        for ycol in xvgdata['legend']:
+            if not ycol == "x": 
+                plt.plot(xvgdata['data'].iloc[:,0], xvgdata['data'][ycol], label = ycol)
+                if average == True:
+                    plt.plot(xvgdata['data'].iloc[:,0], xvgdata['data'][ycol].rolling(window=20).mean(), label = ycol + " (average " + str(averageTimeStep) + ")" )
+        plt.legend()
+    if xvgdata['plotType'] == 'xydy':
+        plt.errorbar(xvgdata['data'].iloc[:,0], xvgdata['data'].iloc[:,1], yerr = xvgdata['data'].iloc[:,2], capsize=5)    
+    plt.title(title)
     plt.xlabel(xlab)
     plt.ylabel(ylab)
-    plt.title(title)
-    plt.legend()
     plt.show()  
+
