7096e2652472394e1d724efa4ee3768a0611ab6b
khabibki06 <48047638+khabibki06@users.noreply.github.com>
2019-09-25 08:29:08 +0700

    Update __init__.py

--- pyGromacs/__init__.py
+++ pyGromacs/__init__.py
@@ -1,1 +1,14 @@
+import sys
 
+
+try:
+    import pandas
+    import matplotlib.pyplot as plt
+    import numpy as np
+except ImportError as e:
+    print('\t{0}'.format(e))
+    sys.exit(1)
+    
+from .read_xvg import math_exp
+from .read_xvg import read_xvg
+from .plot_xvg import plot_xvg
