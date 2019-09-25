8e9dde07f60353ef29cba942689a30a1169353cd
khabibki06 <48047638+khabibki06@users.noreply.github.com>
2019-09-25 08:11:38 +0700

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(
+    # Needed to silence warnings (and to be a worthwhile package)
+    name='Python Gromacs',
+    url='https://github.com/khabibki06/pyGromacs',
+    author='khabib khumaini',
+    author_email='khabibki06@gmail.com',
+    # Needed to actually package something
+    packages=['pyGromacs'],
+    # Needed for dependencies
+    install_requires=['numpy', 'pandas', 'sys', 'matplotlib'],
+    # *strongly* suggested for sharing
+    version='0.1',
+    # The license can be anything you like
+    license='UP',
+    description='An example of a python package from pre-existing code',
+    # We will also need a readme eventually (there will be a warning)
+    # long_description=open('README.txt').read(),
+)
